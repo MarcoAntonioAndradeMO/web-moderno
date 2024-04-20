7e07b5fcbea13a86bd90a367d65a2fd7af034769
Marco Andrade <marcoantonio.andrade05@gmail.com>
2024-04-20 10:41:53 -0300

    Modulos

---
+++ modulo_10_node/pastaA/PastaB/acessarModulos.js
@@ -0,0 +1,11 @@
+const moduloA = require('../../moduloA');
+console.log(moduloA.ola);
+
+// Caminho Absoluto => Deve ser Evitado
+//const moduloA = require('C:\Users\marco\OneDrive\Área de Trabalho\web-moderno\modulo_10_node\moduloA.js');
+
+// const http = require('http')
+// http.createServer((req, res) => {
+//     res.write('Bom Dia!');
+//     res.end()
+// }).listen(8080);
